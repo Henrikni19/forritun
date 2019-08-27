065b81d90d50e0cfbd3351b2c336ef10056e7897
Henrik Marcin Niescier <henrikni19@tskoli.is>
2019-08-27 12:05:33 +0000

    kláraði 5 og 6 og lagaði texta í öllum liðum

--- Verkefni 1/python_Liður 1.py
+++ Verkefni 1/python_Liður 1.py
@@ -1,34 +1,43 @@
 #Liður 1
-afangi = input("afgangi = ")
-nafn = input("Nafn = ")
+afangi = input("Í hvaða áfanga ert þú í = ")
+nafn = input("Hvað er nafnið þitt = ")
 
 print ("Velkomin í áfangann " + afangi + " þetta verður skemmtileg önn hjá okkur " + nafn)
 
 #Liður 2
-print(" Skrifaðu tölu hærri en 100 ")
-tala = int(input("tala = "))
+print(" Skrifaðu tölu hærri en 100 sem verður deilt í 5.5 ")
+tala = int(input("tala = ") or 100)
+
 if tala > 100:
-    print(tala/5.5)
+    print (round(tala/5.5,2))
 else:
     print("invalid number")
 
-#Forritið biður notanda að slá inn tölu hærri en 100 og deilir síðan í hana með 5.5 og birtir svarið með 2 aukastöfum
-
 #Liður 3
-print( "Skrifaðu tvær heiltölur" )
-tala1 = int(input( "tala1 = " ))
-tala2 = int(input( "tala2 = " ))
+print( "Skrifaðu tvær heiltölur sem munu margfaldast og tekið tölu1 frá tölu2" )
+tala1 = int(input( "tala1 = " ) or 1)
+tala2 = int(input( "tala2 = " ) or 1)
 print( tala1 * tala2 )
 print( tala2 - tala1 )
 
-#Forritið biður um tvær heiltölur, það á síðan að margfalda þessar tvær tölur saman og birta útkomuna á skjáinn.  
-#Svo á forritið að draga fyrri töluna sem sleginn var inn frá þeirri seinni og skrifa þá útkomu á skjáinn. 
-
 #liður 4
-print( "hvað eru hliðarlengdirnar á kassanum" )
-hlið1 = float(input("Hlið_L = "))
-hlið2 = float(input("Hlið_B = "))
-hlið3 = float(input("Hlið_H = "))
+print( "Hvað eru hliðarlengdirnar á kassanum" )
+hlið1 = float(input("Hlið_L = ") or 1)
+hlið2 = float(input("Hlið_B = ") or 1)
+hlið3 = float(input("Hlið_H = ") or 1)
 print( "svarið er " + str(hlið1 * hlið2 * hlið3))
 
-#Forritið tekur inn 3 hliðarlengdir á kassa (heiltölur) og skilar rúmmáli kassans til notandans. Formúlan er: Rúmmál = hæð * lengd * breidd.
+#Liður 5
+print("Hér finnum við út hvað faðir þinn var gamall þegar þú fæddist, skrifaðu aldurinn þinn og faðir þíns")
+kid = int(input("Aldurinn þinn = "))
+dad = int(input("Aldur faðirins = "))
+print("Faðir þinn var " + str(dad - kid) + " ára þegar þú fæddist")
+
+#Liður 6
+print("Hér ætlum við að finna út meðaltal á 3 einstaklingum")
+person1 = int(input("Einstaklingur_1 = "))
+person2 = int(input("Einstaklingur_2 = "))
+person3 = int(input("Einstaklingur_3 = "))
+print ("Meðaltal aldurs þeirra er " + str((person1 + person2 + person3) / 3))
+
+print("Gaman að geta aðstoðað þig við þessa útreikninga " + nafn)
