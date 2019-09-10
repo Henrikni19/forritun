108965ff2146ded8057fcb6d356c00cacc18a8f5
Henrik Marcin Niescier <henrikni19@tskoli.is>
2019-09-10 12:50:09 +0000

    klaraði 1 og 2

--- Verkefni 1/python_Liður 1.py
+++ Verkefni 1/python_Liður 1.py
@@ -4,6 +4,7 @@ nafn = input("Hvað er nafnið þitt = ")
 
 print ("Velkomin í áfangann " + afangi + " þetta verður skemmtileg önn hjá okkur " + nafn)
 
+
 #Liður 2
 print(" Skrifaðu tölu hærri en 100 sem verður deilt í 5.5 ")
 tala = int(input("tala = ") or 100)
@@ -13,12 +14,14 @@ if tala > 100:
 else:
     print("invalid number")
 
+
 #Liður 3
 print( "Skrifaðu tvær heiltölur sem munu margfaldast og tekið tölu1 frá tölu2" )
 tala1 = int(input( "tala1 = " ) or 1)
 tala2 = int(input( "tala2 = " ) or 1)
 print( tala1 * tala2 )
 print( tala2 - tala1 )
+
 
 #liður 4
 print( "Hvað eru hliðarlengdirnar á kassanum" )
@@ -27,11 +30,13 @@ hlið2 = float(input("Hlið_B = ") or 1)
 hlið3 = float(input("Hlið_H = ") or 1)
 print( "svarið er " + str(hlið1 * hlið2 * hlið3))
 
+
 #Liður 5
 print("Hér finnum við út hvað faðir þinn var gamall þegar þú fæddist, skrifaðu aldurinn þinn og faðir þíns")
 kid = int(input("Aldurinn þinn = "))
 dad = int(input("Aldur faðirins = "))
 print("Faðir þinn var " + str(dad - kid) + " ára þegar þú fæddist")
+
 
 #Liður 6
 print("Hér ætlum við að finna út meðaltal á 3 einstaklingum")
